520945e44191e499c5cd343baeb6dffbcd539853
William Edmalm <nilswilliam2005@gmail.com>
2026-07-23 19:13:34 +0200

    life

--- DraculaDown/Assets/Script/PlayerLife.cs
+++ DraculaDown/Assets/Script/PlayerLife.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 public class PlayerLife : MonoBehaviour
 {
     [SerializeField] private int life;
-
+    public bool isAlive = true;
     public void DamagePlayer(int damage)
     {
         life -= damage;
-        if (life <= 0) Debug.Log("I AM DEADDD");
+        if (life <= 0)
+        {
+            isAlive = false;
+            Debug.Log("I AM DEADDD");
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+        if (!isAlive) return;
         Debug.Log("COLLISION DETECTED");
         if (other.gameObject.layer != 9)
         {
