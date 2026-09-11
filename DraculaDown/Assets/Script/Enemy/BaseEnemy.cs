3e419a3080b0609f0e628aafb647b2e117f834ba
Edmalm <nilswilliam2005@gmail.com>
2026-09-11 16:04:04 +0200

    uh oh didnt publish from home, check this

--- DraculaDown/Assets/Script/Enemy/BaseEnemy.cs
+++ DraculaDown/Assets/Script/Enemy/BaseEnemy.cs
@@ -7,6 +7,7 @@ public class BaseEnemy : MonoBehaviour
     [SerializeField] protected Transform target;
     [SerializeField] protected float speed;
     [SerializeField] protected float agroRadius;
+    [SerializeField] private float rubberbandingRadius = 8; //The distance to the player for enemy to start speeding up
     
     protected bool isMoving;
     protected bool isRotating;
@@ -35,8 +36,12 @@ public class BaseEnemy : MonoBehaviour
     protected virtual void Movement()
     {
         if (!isMoving) { return; }
+
+        float localSpeed = speed;
+        if (Vector2.Distance(target.position, transform.position) > rubberbandingRadius) localSpeed = speed * 2;
+            
         
-        rb.linearVelocity = transform.up * (speed * Time.deltaTime);
+        rb.linearVelocity = transform.up * (localSpeed * Time.deltaTime);
         
         //TODO The farther away the player is, the enemy should move faster,
         //TODO See mario kart rubberbanding -mvh ALDIN
@@ -55,5 +60,7 @@ public class BaseEnemy : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, agroRadius);
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, rubberbandingRadius);
     }
 }
